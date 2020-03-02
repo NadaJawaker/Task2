085dc17aa5e8b0188da9efe26d8b3198d9986767
Nada <alshamayleh@jawaker.com>
2020-03-02 15:16:11 +0200

    associations for comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
 	belongs_to :user
+	has_many :comments
 	validates :title, presence: true, length: {minimum: 1, maximum: 100}
 	validates :description, presence: true, length: {minimum: 1, maximum: 3000}
 	validates :user_id, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
 	has_many :articles
+	has_many :comments
 	before_save { self.email = email.downcase }
 	
 
