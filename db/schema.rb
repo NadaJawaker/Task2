fad3375421ed2d815115743268b2f81154e55a41
NadaJawaker <alshamayleh@jawaker.com>
2020-03-09 12:35:09 +0200

    emails index and username index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_073435) do
+ActiveRecord::Schema.define(version: 2020_03_09_103113) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +42,8 @@ ActiveRecord::Schema.define(version: 2020_03_04_073435) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
