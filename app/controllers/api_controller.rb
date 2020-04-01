8894cf60a6210d7f8efb50e839327f17608ed95a
NadaJawaker <alshamayleh@jawaker.com>
2020-04-01 23:25:17 +0300

    final toutches for API controller

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -5,6 +5,14 @@ class ApiController < ApplicationController
     rdm = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
     errorEnd = (0...50).map { rdm[rand(rdm.length)] }.join
     code = Submission.find(params["codeId"]).code
+    
+    code = <<-CODE
+      begin
+        #{code}
+      rescue Exception => e
+        puts e.class.to_s + \" #{errorEnd}\"
+      end
+    CODE
 
     output = helpers.funAPI(code, params["input"])
     tmp = output.split(' ').last
