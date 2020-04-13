0a2bd7b44c9c82756fe1b993289cc3148a603d35
NadaJawaker <alshamayleh@jawaker.com>
2020-04-13 23:00:33 +0300

    Fix development environment & some frontend edits

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_203558) do
+ActiveRecord::Schema.define(version: 2020_04_12_151245) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2020_03_15_203558) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.text "gender"
     t.index ["email"], name: "index_users_on_email"
     t.index ["username"], name: "index_users_on_username"
   end
