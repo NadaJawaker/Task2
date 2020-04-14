621f3945ad41044ce9b3607f2dfb3464ef1411b9
NadaJawaker <alshamayleh@jawaker.com>
2020-04-14 17:35:58 +0300

    LaGlaGet el API

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,27 +2,40 @@ class ApiController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:test]
 
   def test
+    @submission = Submission.new
+    @submission.user = User.find(5)
     rdm = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
     errorEnd = (0...50).map { rdm[rand(rdm.length)] }.join
-    code = Submission.find(params["codeId"]).code
-    
-    code = <<-CODE
+    @submission.code = Submission.find(params["code_id"]).code
+    @submission.code = <<-CODE
       begin
-        #{code}
+        #{@submission.code}
       rescue Exception => e
         puts e.class.to_s + \" #{errorEnd}\"
       end
     CODE
 
-    output = helpers.funAPI(code, params["input"])
-    tmp = output.split(' ').last
+    @submission.input= params["input"]
+    @submission.output = helpers.funAPI(@submission.code, @submission.input)
+    tmp = @submission.output.split(' ').last
     if tmp != errorEnd.to_s
-      status = "Code has run successfully :)"
+      @submission.status = "Code has run successfully :)"
     else
-      status = output.split(' ').first
-      output = "No output, there's an error :("
+      @submission.status = @submission.output.split(' ').first
+      @submission.output = "No output, there's an error :("
     end
-    render json: {output: output, status: status},status: :ok
+
+    @submission.code = helpers.toOriginalCode(@submission)
+    @submission.save
+
+    render json: {output: @submission.output, status: @submission.status},status: :ok
   end
 
+
 end
+
+
+
+
+
+
